482b6378a407460704d287fc53f3656e0b189c4e
Max Qiu <lemonade475@163.com>
2020-09-02 16:21:38 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -22,6 +22,5 @@ mkdir openwrt-packages && cd openwrt-packages
 git clone https://github.com/rosywrt/luci-theme-rosy.git
 git clone https://github.com/vernesong/OpenClash.git
 git clone https://github.com/maxlicheng/luci-app-ssr-plus.git
-pwd
 cd /home/runner/work/openwrt-actions/openwrt-actions/openwrt/feeds/luci/applications/
 git clone https://github.com/pymumu/luci-app-smartdns
